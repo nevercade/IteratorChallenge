78f858f72792557eacb3fc6fb5e5cb7b034bd6d9
Matt <nocluegaming@gmail.com>
2021-07-07 13:25:51 +0100

    Source Code

---
+++ IteratorChallenge/IteratorChallenge/MyIterator.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IteratorChallenge {
+    class MyIterator {
+        //Stores a list of numbers iterating over a multiple until they hit a limit.
+        ArrayList nums;
+        int multiple;
+        int limit;
+
+        public MyIterator(int multiple, int limit) {
+            //Constructor Method
+            nums = new ArrayList();
+
+            rebuild(multiple, limit);
+            Console.WriteLine($"--Iterator from {multiple} to {limit} set up.");
+            }
+
+        public void rebuild(int multiple, int limit) {
+            //Builds the iterator using modulo
+            this.multiple = multiple;
+            this.limit = limit;
+
+            for(int i = 1; i <= limit; i++) {
+                if(i % multiple == 0) {
+                    nums.Add(i);
+                    }
+                }
+
+            }
+
+        public void print() {
+            //Prints all of the numbers.
+            Console.WriteLine($"--Printing all numbers in the iterator of {multiple} until {limit}");
+            foreach(int number in nums) { //the numbers are iterated over here.
+                Console.WriteLine(number);
+                }
+            }
+        public IEnumerable getIterator() {
+            //returns the arraylist as an enumerable
+            return nums;
+            }
+
+        }
+    }
---
+++ IteratorChallenge/IteratorChallenge/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IteratorChallenge {
+    class Program {
+
+
+        static void Main(string[] args) {
+
+            //I've built in a function to this iterator that allows you to print through all the numbers that it would iterate through
+            MyIterator iterator1 = new MyIterator(7, 100);
+            MyIterator iterator2 = new MyIterator(8, 200);
+            MyIterator iterator3 = new MyIterator(9, 300);
+
+
+
+
+
+
+
+            iterator1.print(); //Using the print function that I've set up in the iterator. 
+            iterator2.print(); //Using the print function that I've set up in the iterator. 
+
+            IEnumerable enumerator = iterator3.getIterator();  //Now I'm pulling the numbers iterator and excecuting code outside of the MyIterator object. This offers additional flexibility
+            Console.WriteLine("--Now printing directly from inumeration instead of the print list.");
+            foreach(int number in enumerator) { 
+                 Console.WriteLine(number); 
+                }
+            Console.ReadKey();
+
+
+
+
+
+
+            }
+
+
+
+        }
+    }
